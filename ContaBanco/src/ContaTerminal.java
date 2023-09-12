45b428b97babcbb038e60ffc3a08e34db779826f
giomascitelli <giomascitelli@gmail.com>
2023-09-12 17:11:51 -0300

    Commit final do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número de sua conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número de sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo da sua conta:");
+        BigDecimal saldo = scanner.nextBigDecimal();
+
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
